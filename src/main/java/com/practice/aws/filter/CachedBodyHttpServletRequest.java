74990882b2c7e39fb664c9b1ddf4dbd8ef51ba5f
mayuran siva <smayuran81@gmail.com>
2024-07-13 20:34:41 +0100

    change image repository name

---
+++ src/main/java/com/practice/aws/filter/CachedBodyHttpServletRequest.java
@@ -0,0 +1,30 @@
+package com.practice.aws.filter;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.util.StreamUtils;
+
+import java.io.*;
+
+public class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
+
+    private byte[] cachedBody;
+    public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
+
+        super(request);
+        InputStream requestInputStream = request.getInputStream();
+        this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
+}

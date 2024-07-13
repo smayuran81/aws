3d5ff3d35855b2d5dc9243dc3defb2275f55ce3f
mayuran siva <smayuran81@gmail.com>
2024-07-13 20:13:52 +0100

    controller updated

--- src/main/java/com/practice/aws/rest/TestController.java
+++ src/main/java/com/practice/aws/rest/TestController.java
@@ -1,6 +1,8 @@
 package com.practice.aws.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +11,15 @@ public class TestController {
     @GetMapping("/data")
     public String getData() {
 
-        return "first message from AWS Ecs";
+        return "code updated message from AWS Ecs";
 
+    }
+
+    @PostMapping("/testPost")
+    public String getPost(@RequestBody String string) {
+
+        System.out.println("test");
+        return string;
     }
 
 
